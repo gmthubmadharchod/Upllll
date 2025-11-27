dd8da33487618f141557757d83bfac2f443856ea
gmthubmadharchod <sonusinghtomar778@gmail.com>
2025-11-27 23:25:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "37803484"
+api_hash = "75a1cf6929eabe8fb6dd3481387d5f16"
+bot_token = "8259411435:AAGqsgRm1c3nybmMz1eozqfzrfe_AmpD0Xo"
+auth_users = [8398342182]
+sudo_users = [8398342182]
