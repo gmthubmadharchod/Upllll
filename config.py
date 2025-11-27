ab4dc300aaf715c0e7e7677751f0d49a69a35ad6
gmthubmadharchod <sonusinghtomar778@gmail.com>
2025-11-28 00:17:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "37803484"
-api_hash = "75a1cf6929eabe8fb6dd3481387d5f16"
-bot_token = "8259411435:AAGqsgRm1c3nybmMz1eozqfzrfe_AmpD0Xo"
+api_id = "xxxxxx"
+api_hash = "75axxxxxxxx3481387d5f16"
+bot_token = "kdkncnnsixnndsgRm1c3nybmMz1eozqfzrfe_AmpD0Xo"
 auth_users = [8398342182]
 sudo_users = [8398342182]
